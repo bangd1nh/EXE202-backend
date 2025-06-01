09b22393ed22449c4aa6e85087303c864b5d430a
bangd1nh <dinhbang121@gmail.com>
2025-06-01 17:39:15 +0700

    asdfasdf

--- src/controller/photographers.js
+++ src/controller/photographers.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     getAllPhotographers,
     getServiceByPhotographersId,
+    updatePhotographerProfile,
 } from "../service/photographers/index.js";
 import { getPhotographerProfile } from "../service/user/index.js";
 
@@ -33,5 +34,32 @@ photographers.get("/services/:photographerId", async (req, res) => {
         payload: result.payload,
     });
 });
+
+photographers.get("/user/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const result = await getPhotographerProfile(userId);
+    res.status(result.code).json({
+        message: result.message,
+        payload: result.payload,
+    });
+});
+
+photographers.put("/user/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const { experienceYear, location, device, desc } = req.body;
+    const result = await updatePhotographerProfile(
+        userId,
+        experienceYear,
+        location,
+        device,
+        desc
+    );
+    res.status(result.code).json({
+        message: result.message,
+        payload: result.payload,
+    });
+});
+
+photographers.post("/user/uploadImage/:userId", async (req, res) => {});
 
 export default photographers;
--- src/service/photographers/index.js
+++ src/service/photographers/index.js
@@ -27,3 +27,40 @@ export const getServiceByPhotographersId = async (photographerId) => {
     }
     return dataResponse(200, "found", service);
 };
+
+export const getPhotographerProfileByUserId = async (userId) => {
+    const userProfile = await PhotographerProfile.findOne({
+        PhotographerId: userId,
+    })
+        .populate("PhotoGraphs")
+        .populate("Services");
+    if (!userProfile) {
+        return dataResponse(404, "not found", null);
+    }
+    return dataResponse(200, "success", userProfile);
+};
+
+export const updatePhotographerProfile = async (
+    userId,
+    expY,
+    location,
+    device,
+    description
+) => {
+    const userProfile = await PhotographerProfile.findOneAndUpdate(
+        {
+            PhotographerId: userId,
+        },
+        {
+            ExperienceYear: expY,
+            Location: location,
+            Device: device,
+            Description: description,
+        },
+        { new: true }
+    );
+    if (!userProfile) {
+        return dataResponse(404, "not found user", null);
+    }
+    return dataResponse(200, "success", userProfile);
+};
